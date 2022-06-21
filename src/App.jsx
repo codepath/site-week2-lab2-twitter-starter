11c49f63c0e2ef480a394f5c8bcaf37145bc7ffb
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-21 09:55:43 -0700

    UserProfile recieves the correct props

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Navbar from "./components/Navbar/Navbar"
 import UserProfile from "./components/UserProfile/UserProfile"
 import Advertisements from "./components/Advertisements/Advertisements"
 import { codepathUserProfile, firstTweet, navLinks } from "./constants"
+import { useState } from "react"
 
 export default function App() {
+
+  const [userProfile, setUserProfile] = useState(codepathUserProfile);
+
   return (
     <div className="app">
       <Navbar 
         navLinks={navLinks}
         />
       <main>
-        <UserProfile />
+        <UserProfile 
+          userProfile={userProfile}
+          />
         <Feed />
         <Advertisements />
       </main>
