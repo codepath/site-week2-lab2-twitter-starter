8109284fb0a388a33bae55811357c17365ae91c8
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-21 11:05:48 -0700

    Render navLink label

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export function NavLink({ navLink }) {
   return (
     <li className={navLink.className}>
       <i className={navLink.icon}></i>
-      <span></span>
+      <span>{ navLink.label }</span>
     </li>
   )
 }
